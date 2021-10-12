bf3a8a058ad5ffe4aa8c26392785626fc95ff9e9
ntchuisse <89789159+ntchuisse@users.noreply.github.com>
2021-10-11 23:47:55 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var elementOne = document.body.querySelector(".element");
+var elementTwo = document.body.querySelector(".element2")
+var starter = 5;
+var starter2 = 1;
+var list = [
+    {
+        name: "Harry",
+        age: 25
+    },
+    {
+        name: "Larry",
+        age: 15
+    },
+    {
+        name: "Bert",
+        age: 45
+    },
+    {
+        name: "Jerry",
+        age: 35
+    },
+    {
+        name: "Berry",
+        age: 5
+    },
+    {
+        name: "Bart",
+        age: 55
+    }
+];
+
+for (var i = 0; i < list.length; i++) {
+    var ele = document.createElement("div");
+    var nameEle = document.createElement("h1");
+    var ageEle = document.createElement("h2");
+    nameEle.innerHTML = "Name: " + list[i].name;
+    ageEle.innerHTML = "Age: " + list[i].age;
+    if (age > 25) {
+        ageEle.style.color="green"
+    }
+
+}
+ele.appendChild(nameEle);
+ele.appendChild(ageEle);
+wrapperEle.appendChild
+element2.innerHTML = starter
+document.body.querySelector("button"); addEventListener("click")
+
+starter = starter + starter2;
+el
